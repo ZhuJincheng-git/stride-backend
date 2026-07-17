477475e328b575d51b9ce41300ee9dff65975dc2
ZhuJincheng-git <ma2333chao@gmail.com>
2026-07-17 11:58:23 +0800

    docs: add comments for migrate.go

--- internal/model/migrate.go
+++ internal/model/migrate.go
@@ -2,12 +2,14 @@ package model
 
 import "gorm.io/gorm"
 
+// AllModels returns every model registered with GORM.
 func AllModels() []any {
 	return []any{
 		&User{},
 	}
 }
 
+// AutoMigrate runs `db.AutoMigrate` on every registered model.
 func AutoMigrate(db *gorm.DB) error {
 	return db.AutoMigrate(AllModels()...)
 }
