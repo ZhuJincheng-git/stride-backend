2b4f9dfd25e58a44acd29ab2fd144072838e342f
ZhuJincheng-git <ma2333chao@gmail.com>
2026-07-17 13:41:38 +0800

    feat: implement user_repo.go repository layer

---
+++ internal/repository/user_repo.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+
+	"github.com/ZhuJincheng-git/stride-backend/internal/model"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, u *model.User) error
+	GetByID(ctx context.Context, id uuid.UUID) (*model.User, error)
+	GetByUsername(ctx context.Context, username string) (*model.User, error)
+	GetByEmail(ctx context.Context, email string) (*model.User, error)
+}
+
+type userRepo struct { db *gorm.DB }
+
+func NewUserRepository(db *gorm.DB) UserRepository { return &userRepo{db: db} }
+
+func (r *userRepo) 	Create(ctx context.Context, u *model.User) error {
+	return r.db.WithContext(ctx).Create(u).Error
+}
+
+func (r *userRepo) GetByID(ctx context.Context, id uuid.UUID) (*model.User, error) {
+	var u model.User
+	if err := r.db.WithContext(ctx).First(&u, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (r *userRepo) GetByUsername(ctx context.Context, username string) (*model.User, error) {
+	var u model.User
+	if err := r.db.WithContext(ctx).First(&u, "username = ?", username).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (r *userRepo) GetByEmail(ctx context.Context, email string) (*model.User, error) {
+	var u model.User
+	if err := r.db.WithContext(ctx).First(&u, "email = ?", email).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
