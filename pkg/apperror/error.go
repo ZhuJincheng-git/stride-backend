a8b54a91935f166286844ff4f2b47e925dbe1725
ZhuJincheng-git <ma2333chao@gmail.com>
2026-07-20 13:34:00 +0800

    feat: init custom error package skeleton

---
+++ pkg/apperror/error.go
@@ -0,0 +1,42 @@
+package apperror
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"golang.org/x/text/message"
+)
+
+type Code string
+
+const (
+	CodeInvalidArgument  Code = "invalid_argument"
+	CodeUnauthenticated  Code = "unauthenticated"
+	CodePermissionDenied Code = "permission_denied"
+	CodeNotFound         Code = "not_found"
+	CodeInternal         Code = "internal"
+)
+
+type Error struct {
+	Code Code // machine-readable
+	Message string // human-readable, show to API clients
+	cause error // optional underlying error (NOT exposed to clients)
+}
+
+func New(code Code, message string) *Error {
+	return &Error{Code: code, Message: message}
+}
+
+func Wrap(code Code, message string, cause error) *Error {
+	return &Error{Code: code, Message: message, cause: cause}
+}
+
+// Error is used for logging.
+func (e *Error) Error() string {
+	if e.cause != nil {
+		return fmt.Sprintf("%s: %s: %v", e.Code, e.Message, e.cause)
+	}
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
