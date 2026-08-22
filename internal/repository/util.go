22817ee9414fc28f6daa5b436f6ca73a8bf3bbed
ZhuJincheng-git <ma2333chao@gmail.com>
2026-08-22 17:52:41 +0800

    fix: add missing newline at end of file in util.go

--- internal/repository/util.go
+++ internal/repository/util.go
@@ -4,6 +4,20 @@ import (
 	"errors"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func IsNotFound(err error) bool { return errors.Is(err, gorm.ErrRecordNotFound) }
+
+// onConflictDoNothing builds the clause used by bulk-inserts that should be
+// idempotent: "INSERT ... ON CONFLICT(<cols>) DO NOTHING".
+func onConflictDoNothing(cols ...string) []clause.Expression {
+	c := clause.OnConflict{
+		Columns: make([]clause.Column, 0, len(cols)),
+		DoNothing: true,
+	}
+	for _, col := range cols {
+		c.Columns = append(c.Columns, clause.Column{Name: col})
+	}
+	return []clause.Expression{c}
+}
